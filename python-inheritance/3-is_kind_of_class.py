fdafb2957e5108d0c79002cc2a352eafb7a4af3f
UmwariVanessaLavie <v.umwari@alustudent.com>
2025-03-07 23:42:41 +0200

    h

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 '''A script that checks if the object is an instance of a class'''
 
-#!/usr/bin/python3
-'''A script that checks if the object is an instance of a class'''
-
 
 def is_kind_of_class(obj, a_class):
     '''checks if the object is an instance of a class
@@ -17,15 +14,3 @@ def is_kind_of_class(obj, a_class):
        False: if the obj isn't an instance of, or if the it is a class instance
     '''
     return isinstance(obj, a_class)
-
-
-
-
-
-
-
-
-
-
-
-
