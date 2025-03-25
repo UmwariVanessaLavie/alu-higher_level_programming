04febf6844fbdd3908a53cf185474aa7d9309572
UmwariVanessaLavie <v.umwari@alustudent.com>
2025-03-25 14:10:24 +0200

    k

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -14,4 +14,4 @@ with urllib.request.urlopen(req) as response:
     print("Body response:")
     print("\t- type:", type(content))
     print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+    print("\t- utf8 content:", content.decode("utf-8")
